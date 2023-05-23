064e044add76ce6c44af1d3f590ee649a8a710f5
Jayanti2919 <jayanti2919@gmail.com>
2023-05-23 10:24:06 +0530

    changed api route

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -55,7 +55,7 @@ const Customizer = () => {
     try{
       setgeneratingImg(true)
       console.log(prompt)
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+      const response = await fetch('https://threejs-ai-tshirt-iiqd.onrender.com/api/v1/dalle', {
         method:'POST',
         headers: {
           'Content-Type': "application/json", 
